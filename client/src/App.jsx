cf3a4d4e6ef42c72b9e4fa2083fbc9626477ff00
Akshatpathania <akshatpathania.0333@gmail.com>
2026-02-22 06:20:39 +0530

    connect frontend to render backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { io } from "socket.io-client";
 import "./App.css";
 
-const socket = io(`http://${window.location.hostname}:5000`);
+const socket = io("https://mern-multiplayer-game.onrender.com");
 
 function App() {
   const [roomId, setRoomId] = useState("");
